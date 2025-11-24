bc35e44b7a19634cb4643fd72d4486ab3d0984f7
mattyyioo <mefu2232@gmail.com>
2025-11-24 13:14:26 +0100

    Add comments describing the code and use of poll function

--- Projekt.c
+++ Projekt.c
@@ -287,9 +287,5 @@ int main(int argc, char *argv[])
     free(train_data);
     free(test_data);
     printf("[main]Koncze dzialanie\n");
-    printf("[main]Koncze dzialanie\n");
-    printf("[main]Koncze dzialanie\n");
-    printf("[main]Koncze dzialanie\n");
-    printf("[main]Koncze dzialanie\n");
     return 0;
 }
